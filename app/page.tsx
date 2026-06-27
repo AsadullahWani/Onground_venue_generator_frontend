2b78c09c0e9666fd58087caa81a19c3d794ff5d1
Asad <86241123+AsadullahWani@users.noreply.github.com>
2026-06-28 00:05:56 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -34,7 +34,7 @@ export default function VenueFinder() {
 
     try {
       const response = await fetch(
-        "https://52.141.48.77:7000/venues/search/",
+        "http://52.141.48.77:7000/venues/search/",
         {
           method: "POST",
           headers: {
